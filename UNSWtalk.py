e1bd5d7bab83728670fda1710d68a4817927e478
z5128684 <z5128684@cse.unsw.edu.au>
2017-10-23 23:41:52 +1100

    add bootstrap style, hide private information

--- UNSWtalk.py
+++ UNSWtalk.py
@@ -8,9 +8,20 @@ import os
 from flask import Flask, render_template, session, send_from_directory, url_for
 
 students_dir = "dataset-medium";
+key_not_show = ['email','password','home_latitude','home_longitude','courses']
+key_to_show = ['zid', 'program', 'birthday', 'home_suburb', 'friends']
 
 app = Flask(__name__)
 
+# read user file
+def read_user_file(student_to_show):
+    details_filename = os.path.join(students_dir, student_to_show, "student.txt")
+    with open(details_filename) as f:
+        details = f.readlines()
+    for line in details:
+        key, val = line.split(':',1)
+        session[key] = val
+    
 # Show unformatted details for student "n"
 # Increment n and store it in the session cookie
 
@@ -23,16 +34,17 @@ def start():
         n = 0
     students = sorted(os.listdir(students_dir))
     student_to_show = students[n % len(students)]
-    details_filename = os.path.join(students_dir, student_to_show, "student.txt")
-    with open(details_filename) as f:
-        details = f.read()
+    read_user_file(student_to_show)
     img_path = os.path.join(student_to_show, "img.jpg")
     session['n'] = n + 1
-    return render_template('start.html', student_details=details, img=img_path)
+    return render_template('start.html', 
+                            img=img_path)
 
 @app.route('/image/<path:filename>')
 def custom_static(filename):
     return send_from_directory(students_dir, filename)
+
+
 
 if __name__ == '__main__':
     app.secret_key = os.urandom(12)
