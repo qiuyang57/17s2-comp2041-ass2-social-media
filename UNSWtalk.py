9ea21a277aa9d80f56c9fa4ff43f607bd93058d0
qiuyang57 <qiuyang57@gmail.com>
2017-10-31 05:42:37 +1100

    change view to match db

--- UNSWtalk.py
+++ UNSWtalk.py
@@ -4,331 +4,82 @@
 # as a starting point for COMP[29]041 assignment 2
 # https://cgi.cse.unsw.edu.au/~cs2041/assignments/UNSWtalk/
 
-import os, re
-from collections import OrderedDict
+import os
+import sqlite3
+from functools import wraps
+
+from flask import g
 from flask import Flask, render_template, session, send_from_directory, request, url_for, redirect
 
-students_dir = "dataset-medium"
-key_hidden = ['email', 'password', 'home_latitude', 'home_longitude', 'courses', 'n']
-check_icon = set()
-zid_name_dict={}
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+
+
+data_folder_name = "dataset-medium"
 
 app = Flask(__name__)
+app.config.from_object(__name__)
+
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, 'database/{}.db'.format(data_folder_name)),
+    SECRET_KEY='yang_qiu',
+    USERNAME='admin',
+    PASSWORD='default'
+))
+# app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+
+# Database
 
 
-# User class
-class User:
-    def __init__(self, zid):
-        self.zid = zid
-        self.full_name = ''
-        self.have_icon = have_icon(self.zid)
-        if self.have_icon:
-            check_icon.add(self.zid)
-        self.info_dict = OrderedDict([('program', ''), ('birthday', ''), ('home_suburb', '')])
-        self.friends = {}
-        # init
-        self.read_user_file()
-        self.posts = []
+Base = automap_base()
+engine = create_engine("sqlite:///database/{}.db".format(data_folder_name))
 
-    # read user posts
-    # input: student zid
-    # return: a list of Post Objects
-    def read_post(self):
-        i = 0
-        posts = []
-        post_path = students_dir + '/' + self.zid + '/' + str(i) + '.txt'
-        while os.path.isfile(post_path):
-            with open(post_path, encoding="utf8") as f:
-                longitude = ""
-                latitude = ""
-                for line in f.readlines():
-                    key, val = line.split(':', 1)
-                    key = key.strip()
-                    val = val.strip()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+User = Base.classes.user
+Friend = Base.classes.friends
+Enrollment = Base.classes.enrollment
+Post = Base.classes.post
+Comment = Base.classes.comment
+Reply = Base.classes.reply
+db_session = Session(engine)
 
-                    if key == 'from':
-                        zid_from = val
-                    elif key == 'time':
-                        time = val
-                    elif key == 'message':
-                        r = '<br />'
-                        val = val.replace('\\n', r)
-                        message = val
-                    elif key == 'longitude':
-                        longitude = val
-                    elif key == 'latitude':
-                        latitude = val
-            post = Post(zid_from, time, message, longitude, latitude)
-            post.have_icon = self.have_icon
-            posts.append(post)
+# some global data
+key_hidden = ['email', 'password', 'home_latitude', 'home_longitude', 'courses']
 
-            j = 0
-            comment_path = students_dir + '/' + self.zid + '/' + str(i) + '-' + str(j) + '.txt'
-            while os.path.isfile(comment_path):
-                with open(comment_path, encoding="utf8") as f:
-                    for line in f.readlines():
-                        key, val = line.split(':', 1)
-                        key = key.strip()
-                        val = val.strip()
-                        if key == 'from':
-                            zid_from = val
-                        elif key == 'time':
-                            time = val
-                        elif key == 'message':
-                            message = val
-                post.insert_comment(zid_from, time, message)
-                comment = post.comments[-1]
-
-                k = 0
-                reply_path = students_dir + '/' + self.zid + '/' + str(i) + '-' + str(j) + '-' + str(k) + '.txt'
-                while os.path.isfile(reply_path):
-                    with open(reply_path, encoding="utf8") as f:
-                        for line in f.readlines():
-                            key, val = line.split(':', 1)
-                            key = key.strip()
-                            val = val.strip()
-                            if key == 'from':
-                                zid_from = val
-                            elif key == 'time':
-                                time = val
-                            elif key == 'message':
-                                message = val
-                    comment.insert_reply(zid_from, time, message)
-                    k += 1
-                    reply_path = students_dir + '/' + self.zid + '/' + str(i) + '-' + str(j) + '-' + str(k) + '.txt'
-
-                j += 1
-                comment_path = students_dir + '/' + self.zid + '/' + str(i) + '-' + str(j) + '.txt'
-
-            i += 1
-            post_path = students_dir + '/' + self.zid + '/' + str(i) + '.txt'
-        self.posts = posts
-
-    # read user file
-    # and init self.info_dict and self.friends
-    def read_user_file(self):
-        details_filename = os.path.join(students_dir, self.zid, "student.txt")
-        with open(details_filename, encoding="utf8") as f:
-            details = f.readlines()
-        for line in details:
-            key, val = line.split(':', 1)
-            key = key.strip()
-            val = val.strip()
-            if key != 'zid':
-                if key == 'friends':
-                    for friend_zid in val[1:-1].split(','):
-                        friend_zid = friend_zid.strip()
-                        self.friends[friend_zid] = Friend(friend_zid)
-                elif key == 'full_name':
-                    self.full_name = val
-                else:
-                    self.info_dict[key] = val
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(app.config['DATABASE'])
+    return db
 
 
-class Friend:
-    def __init__(self, zid):
-        self.zid = zid
-        self.full_name = ''
-        self.have_icon=have_icon(self.zid)
-        if self.have_icon:
-            check_icon.add(self.zid)
-        self.read_friend_file()
-        self.posts = self.read_friend_post()
-
-    def read_friend_post(self):
-        i = 0
-        posts = []
-        post_path = students_dir + '/' + self.zid + '/' + str(i) + '.txt'
-        while os.path.isfile(post_path):
-            with open(post_path, encoding="utf8") as f:
-                longitude = ""
-                latitude = ""
-                for line in f.readlines():
-                    key, val = line.split(':', 1)
-                    key = key.strip()
-                    val = val.strip()
-
-                    if key == 'from':
-                        zid_from = val
-                    elif key == 'time':
-                        time = val
-                    elif key == 'message':
-                        r = '<br />'
-                        val = val.replace('\\n', r)
-                        message = val
-                    elif key == 'longitude':
-                        longitude = val
-                    elif key == 'latitude':
-                        latitude = val
-            post = Post(zid_from, time, message, longitude, latitude)
-            post.have_icon = self.have_icon
-            posts.append(post)
-
-            j = 0
-            comment_path = students_dir + '/' + self.zid + '/' + str(i) + '-' + str(j) + '.txt'
-            while os.path.isfile(comment_path):
-                with open(comment_path, encoding="utf8") as f:
-                    for line in f.readlines():
-                        key, val = line.split(':', 1)
-                        key = key.strip()
-                        val = val.strip()
-                        if key == 'from':
-                            zid_from = val
-                        elif key == 'time':
-                            time = val
-                        elif key == 'message':
-                            message = val
-                post.insert_comment(zid_from, time, message)
-                comment = post.comments[-1]
-
-                k = 0
-                reply_path = students_dir + '/' + self.zid + '/' + str(i) + '-' + str(j) + '-' + str(k) + '.txt'
-                while os.path.isfile(reply_path):
-                    with open(reply_path, encoding="utf8") as f:
-                        for line in f.readlines():
-                            key, val = line.split(':', 1)
-                            key = key.strip()
-                            val = val.strip()
-                            if key == 'from':
-                                zid_from = val
-                            elif key == 'time':
-                                time = val
-                            elif key == 'message':
-                                message = val
-                    comment.insert_reply(zid_from, time, message)
-                    k += 1
-                    reply_path = students_dir + '/' + self.zid + '/' + str(i) + '-' + str(j) + '-' + str(k) + '.txt'
-
-                j += 1
-                comment_path = students_dir + '/' + self.zid + '/' + str(i) + '-' + str(j) + '.txt'
-
-            i += 1
-            post_path = students_dir + '/' + self.zid + '/' + str(i) + '.txt'
-        return posts
-
-    def read_friend_file(self):
-        details_filename = os.path.join(students_dir, self.zid, "student.txt")
-        with open(details_filename, encoding="utf8") as f:
-            details = f.readlines()
-        for line in details:
-            key, val = line.split(':', 1)
-            key = key.strip()
-            val = val.strip()
-            if key == 'full_name':
-                self.full_name = val
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
 
 
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
-class Post:
-    def __init__(self, zid, time, message, longitude='', latitude=''):
-        self.zid = zid
-        self.time = time
-        self.message = message
-        self.have_icon = False
-        self.longitude = longitude
-        self.latitude = latitude
-        self.comments = []
-        self.full_name=""
-        self._name_init()
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if g.user is None:
+            return redirect(url_for('login', next=request.url))
+        return f(*args, **kwargs)
+    return decorated_function
 
-    def _name_init(self):
-        if self.zid not in zid_name_dict:
-            with open(students_dir+'/'+self.zid+'/student.txt', encoding="utf8") as f:
-                for line in f.readlines():
-                    key, val = line.split(':', 1)
-                    key = key.strip()
-                    val = val.strip()
-                    if key == 'full_name':
-                        self.full_name = val
-                        zid_name_dict[self.zid] = self.full_name
-        else:
-            self.full_name=zid_name_dict[self.zid]
-
-
-    def insert_comment(self, zid, time, message=""):
-        self.comments.append(Comment(zid, time, message))
-
-
-class Comment:
-    def __init__(self, zid, time, message):
-        self.zid = zid
-        self.time = time
-        self.message = message
-        if self.zid in check_icon:
-            self.have_icon = True
-        else:
-            self.have_icon = False
-        self.replies = []
-        self.full_name=""
-        self._name_init()
-
-    def _name_init(self):
-        if self.zid not in zid_name_dict:
-            with open(students_dir+'/'+self.zid+'/student.txt', encoding="utf8") as f:
-                for line in f.readlines():
-                    key, val = line.split(':', 1)
-                    key = key.strip()
-                    val = val.strip()
-                    if key == 'full_name':
-                        self.full_name = val
-                        zid_name_dict[self.zid]=self.full_name
-        else:
-            self.full_name=zid_name_dict[self.zid]
-
-    def insert_reply(self, zid, time, message=""):
-        self.replies.append(Reply(zid, time, message))
-
-
-class Reply:
-    def __init__(self, zid, time, message):
-        self.zid = zid
-        self.time = time
-        self.message = message
-        if self.zid in check_icon:
-            self.have_icon = True
-        else:
-            self.have_icon = False
-        self.full_name=""
-        self._name_init()
-
-    def _name_init(self):
-        if self.zid not in zid_name_dict:
-            with open(students_dir+'/'+self.zid+'/student.txt', encoding="utf8") as f:
-                for line in f.readlines():
-                    key, val = line.split(':', 1)
-                    key = key.strip()
-                    val = val.strip()
-                    if key == 'full_name':
-                        self.full_name = val
-                        zid_name_dict[self.zid] = self.full_name
-        else:
-            self.full_name=zid_name_dict[self.zid]
-
-
-
-# read user file
-# save it in session
-
-def have_icon(zid):
-    if os.path.isfile(students_dir + '/' + zid + '/img.jpg'):
-        return True
-    else:
-        return False
-
-# Show unformatted details for student "n"
-# Increment n and store it in the session cookie
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/home', methods=['GET', 'POST'])
 def home():
-    # if 'n' in session:
-    #     n = session['n']
-    # else:
-    #     n = 0
-    # students = sorted(os.listdir(students_dir))
-    # student_to_show = students[n % len(students)]
-    # user = User(student_to_show)
-    # session['n'] = n + 1
     if 'logged_in' in session:
         zid=session['logged_in']
         user=User(zid)
@@ -342,37 +93,50 @@ def home():
 
 @app.route('/image/<path:filename>')
 def custom_static(filename):
-    return send_from_directory(students_dir, filename)
+    return send_from_directory(data_folder_name, filename)
 
+@app.route('/profile_img/<zid>')
+def profile_img(zid):
+    return send_from_directory('static/profile_img/'+zid, 'img.jpg')
 
-@app.route('/login', methods=['POST'])
+@app.route('/login', methods=['POST','GET'])
 def login():
-    zid = request.form.get('zid', '')
-    password = request.form.get('password', '')
-    # store zid in session cookie
-    if zid in os.listdir(students_dir):
-        user = User(zid)
-        if user.info_dict['password']==password:
-            session['logged_in']=zid
-            session['full_name']=user.full_name
-            return redirect(url_for('home'))
-    return render_template('login.html',error='Wrong zid and password combination!')
+    error=''
+    if g.user:
+        return redirect(url_for('home'))
+    if request.method == 'POST':
+        zid = request.form.get('zid', '')
+        password = request.form.get('password', '')
+        user = User.query.filter(User.zid == zid)
+        # store zid in session cookie
+        if user is None:
+            user_sus = User_suspend.query.filter(User.zid == zid)
+            if user:
+                error = 'User suspended!'
+            else:
+                error = 'Please sign up.'
+        elif user.password==password:
+                session['logged_in']=zid
+                session['full_name']=user.full_name
+                return redirect(url_for('home'))
+        else:
+            error='Wrong zid and password combination!'
+    return render_template('login.html',error=error)
 
 @app.route('/logout')
 def logout():
     if 'logged_in' in session:
         session.pop('logged_in')
         session.pop('full_name')
-    return render_template('login.html',error='')
+    return render_template('login.html',error='You successful logout!')
 
-@app.route('/zid/<friend_zid>', methods=['POST','GET'])
-def friends_page(friend_zid):
+@app.route('/zid/<user_zid>', methods=['POST', 'GET'])
+def user_page(user_zid):
     if 'logged_in' in session:
-        friend=User(friend_zid)
-        friend.read_post()
+        user=User.query.filter(User.zid==user_zid)
         return render_template('start.html',
                                key_hidden=key_hidden,
-                               user=friend)
+                               user=user)
     else:
         return render_template('login.html',error='')
 
